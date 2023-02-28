a67e6164e057bded7aabfe4991e807d4b7e86624
Tommaso_Dighero <digghe97@gmail.com>
2023-02-28 15:29:02 +0100

    small fix

--- spoodl/playlist.py
+++ spoodl/playlist.py
@@ -5,6 +5,7 @@ from typing import List
 from dataclasses import dataclass
 from colorama import Fore
 from os import environ as env
+from enum import Enum
 
 SPOTIPY = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(
     client_id=env.get("CLIENT_ID"),
@@ -20,6 +21,7 @@ class Track:
     date: datetime = None
     cover: str = ""
     bpm: str = ""
+    key: int = -1
 
     def query(self) -> str:
         return self.title + " " + str.join(" ", self.artists)
@@ -29,19 +31,38 @@ class Playlist:
     name: str
     tracks: List[Track]
 
+#probably not the best approach
+keylist = {
+    -1: "-",
+    0 : "C",
+    1 : "C♯,D♭",
+    2 : "D",
+    3 : "D♯,E♭",
+    4 : "E",
+    5 : "F",
+    6 : "F♯,G♭",
+    7 : "G",
+    8 : "G♯,A♭",
+    9 : "A",
+    10: "A♯,B♭",
+    11: "B"
+}
+
 def lookup_playlist(playlist: List[str]) -> Playlist:
     name = SPOTIPY.playlist(playlist,"name")["name"]
     playlist_tracks = SPOTIPY.playlist_items(playlist,"items(track(name,artists(name),album,id))")["items"]
     tracks = []
     for track in playlist_tracks:
+        analysis = SPOTIPY.audio_analysis(track["track"]["id"])
         tracks.append(
             Track(
-                title=track["track"]["name"],
-                artists=[ x["name"] for x in track["track"]["artists"] ],
-                album=track["track"]["album"]["name"],
-                date=datetime.strptime(track["track"]["album"]["release_date"], "%Y-%m-%d"),
-                cover=track["track"]["album"]["images"][1]["url"],
-                bpm=int(float(SPOTIPY.audio_analysis(track["track"]["id"])["track"]["tempo"]))
+                title = track["track"]["name"],
+                artists = [ x["name"] for x in track["track"]["artists"] ],
+                album = track["track"]["album"]["name"],
+                date = datetime.strptime(track["track"]["album"]["release_date"], "%Y-%m-%d"),
+                cover = track["track"]["album"]["images"][1]["url"],
+                bpm = int(float(analysis["track"]["tempo"])),
+                key = keylist[int(analysis["track"]["key"])]
             )
         )
         print(
--- spoodl/postprocess.py
+++ spoodl/postprocess.py
@@ -8,14 +8,19 @@ from .playlist import Track
 def tag_mp3_file(track: Track, file: str, extended:bool = False):
     mp3file = eyed3.load(file)
 
+    #comment if extended version
     if extended:
         mp3file.tag.comments.set(u"(extended version)")
+
+    #standard tags
     mp3file.tag.title = track.title
     mp3file.tag.artist = ",".join(track.artists)
     mp3file.tag.album = track.album
     mp3file.tag.date = track.date
     mp3file.tag.bpm = track.bpm
+    mp3file.tag.key = track.key
 
+    #download and embed cover image
     resp = urllib.request.urlopen(track.cover)
     cover = resp.read()
     mp3file.tag.images.set(3, cover, "image/jpeg", u"cover")
